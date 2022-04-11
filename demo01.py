6e3374404cf14d722f57321abc591abfea83414a
Eurus.xu <Eurus_xj@126.com>
2022-04-11 11:33:50 +0800

    第二次

--- demo01.py
+++ demo01.py
@@ -1,3 +1,31 @@
+
+""""
+print("hello world") #字符串
+print(2333) #整数
+print(2.33) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+print(2>1)
+name="zhangsan"
+print(name)
+"""
+# a=float(input("请输入a:"))
+# b=float(input("请输入b:"))
+# print("input获取的内容:",(a+b))
+
+#练习：通过代码获取两段内容，并且计算其长度之和
+from base64 import b16decode
+
+
+# a=str("今天不学习")
+# b=str("明天变垃圾")
+# print(a,b)
+# print (len(a+b))
+a=input("请输入a:")
+b=input("请输入b:")
+print ("长度是：",len(a+b))
 """"
 print("hello world") #字符串
 print(2333) #整数
